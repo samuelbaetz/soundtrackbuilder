d90f0b469e69b5c13b3a7dbd4d8776f33b63644f
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-15 18:24:12 -0500

    content2

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err,data) {
 
     
     
-  spotifyApi.searchTracks($('#search1').val()).then(
+  spotifyApi.searchTracks($('#search1').val(),
       
     function (data) {
         
@@ -82,7 +82,7 @@ spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err,data) {
       });
       console.log(artist)
       console.log(results)
-      $('#soundtrack').html('<p>' + results + " " + artist + '</p>')
+      $('#soundtrack').html('<p>' + results + " " + artist +'</p>')
         }
       },
       function (err) {
