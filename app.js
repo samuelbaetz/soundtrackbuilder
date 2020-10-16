bb71677fa1622458d660e25fc23fc305497e79a7
Richard Walter <richardwalter515@gmail.com>
2020-10-15 21:10:25 -0400

    comment out lyrics empty

--- app.js
+++ app.js
@@ -52,12 +52,12 @@ spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err,data) {
   });
   
  
-  function clearLyrics() {
-    $('#lyricsDisplay').empty();
-  }
+  // function clearLyrics() {
+  //   $('#lyricsDisplay').empty();
+  // }
 
   $(document).on("click", "#search", function(){
-    clearLyrics();
+    // clearLyrics();
     
     
   spotifyApi.searchTracks($('#search1').val()).then(
