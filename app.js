427ed5632e8b8a1c5ff9e309c2615c9061c556e4
Samuel Baetz <samuelbaetz105@gmail.com>
2020-10-19 13:13:33 -0500

    revert

--- app.js
+++ app.js
@@ -96,6 +96,31 @@ spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err,data) {
             
           })
 
+          $('#c').on("click", function(){
+            // console.log(this.text());
+            // var trackFull=this.text();
+            // var trackSplit=trackFull.split("-");
+            // var trackTitle=trackSplit[0];
+            // var trackArtist=trackSplit[1];
+            // console.log(trackFull);
+            // console.log(trackSplit);
+            // console.log(trackTitle);
+            // console.log(trackArtist);
+            // $('#lyricsDisplay').empty();
+            
+            var queryURL = "https://orion.apiseeds.com/api/music/lyric/" + artist + "/" + results + "?apikey=SBJNOlBRhfayoBjkQVpzhTc79xTG4qAyVlnG9WsYOFtxkpoFELDxJsSejr16yC0o "
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+          
+            }).then(function(response) {
+                var trackName = $('<p>').text("Track: " + response.result.track.name);
+                var trackLyrics = $('<p>').text("Lyrics: " + response.result.track.text);
+                $('#lyricsDisplay').append(trackName, trackLyrics);
+            });
+            
+          })
+
       },
       function (err) {
         console.error(err);
